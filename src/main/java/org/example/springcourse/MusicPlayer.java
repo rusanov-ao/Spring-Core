dd3fa30ab780f497772e779b669adbc4767b407a
alex <rusanov-ao@inbox.ru>
2025-08-14 00:38:30 +0300

    up

--- src/main/java/org/example/springcourse/MusicPlayer.java
+++ src/main/java/org/example/springcourse/MusicPlayer.java
@@ -2,12 +2,31 @@ package org.example.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
 
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 }
